ac91013f93197076b26a97e6fc61ed068ff97bfa
nlok5923 <nlokhande5923@gmail.com>
2024-02-21 23:26:06 +0530

    computed r0, r1, r2

--- fflonk_verifier/src/main.rs
+++ fflonk_verifier/src/main.rs
@@ -23,52 +23,59 @@
 //                                          "0x1a193153f3cf956d68aab8afe447f0f6ba985f40edba7d9375b9368f1c55f31b", 
 //                                            "0x0e1a49d180902645b8954552c99af04aed9315725b32ac2623965f887a7a5849" ]
 
-use ethers::types::U256;
-use std::str::FromStr;
+// use ethers::types::U256;
+// use std::str::FromStr;
 
 fn main() {
-    let hex_value = "0x045c6d18f3e18cc4aac314316a47f0010ba8b1035b7dab678933e5bcd248f72b"; // Your hex value
-    let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+    // let hex_value = "0x045c6d18f3e18cc4aac314316a47f0010ba8b1035b7dab678933e5bcd248f72b"; // Your hex value
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
 
-    println!("U256 value: {}", u256_value);
+    // println!("U256 value: {}", u256_value);
 
-    let hex_value = "0x1cc4edb75ac4f07466f70f097d263bad6ca1e1e506bccbccd7de94b39dd3a05e"; // Your hex value
-    let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+    // let hex_value = "0x1cc4edb75ac4f07466f70f097d263bad6ca1e1e506bccbccd7de94b39dd3a05e"; // Your hex value
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
 
-    println!("U256 value: {}", u256_value);
+    // println!("U256 value: {}", u256_value);
 
-    let hex_value = "0x0e11ad74cead5ed3d142083b8ad873acf20cb6e39059d88e61a84982386a698a"; // Your hex value
-    let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+    // let hex_value = "0x0e11ad74cead5ed3d142083b8ad873acf20cb6e39059d88e61a84982386a698a"; // Your hex value
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
 
-    println!("U256 value: {}", u256_value);
+    // println!("U256 value: {}", u256_value);
 
-    let hex_value = "0x23806e3836d9fb0467b1ade5a51564aeda10cc9b97f596456cafa149f6c9bfbb"; // Your hex value
-    let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+    // let hex_value = "0x23806e3836d9fb0467b1ade5a51564aeda10cc9b97f596456cafa149f6c9bfbb"; // Your hex value
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
 
-    println!("U256 value: {}", u256_value);
+    // println!("U256 value: {}", u256_value);
 
-    let hex_value = "0x1aec381b720257b672376b5ccbd1fe787247e3cc7b2c6ec2d171be1c5c1a5837"; // Your hex value
-    let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+    // let hex_value = "0x1aec381b720257b672376b5ccbd1fe787247e3cc7b2c6ec2d171be1c5c1a5837"; // Your hex value
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
 
-    println!("U256 value: {}", u256_value);
+    // println!("U256 value: {}", u256_value);
 
-    let hex_value = "0x038d7d45919eee4e6c7e47c586b1d55e32d102e56d64513575d94a9aeba30241"; // Your hex value
-    let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+    // let hex_value = "0x038d7d45919eee4e6c7e47c586b1d55e32d102e56d64513575d94a9aeba30241"; // Your hex value
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
 
-    println!("U256 value: {}", u256_value);
+    // println!("U256 value: {}", u256_value);
 
 
-    let hex_value = "0x1ba4459cca4bb8b75808d4f38598e4b6cf6a8577f7294def8e85b5955abc681a"; // Your hex value
+    // let hex_value = "0x1ba4459cca4bb8b75808d4f38598e4b6cf6a8577f7294def8e85b5955abc681a"; // Your hex value
 
-    let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
 
-    println!("U256 value: {}", u256_value);
+    // println!("U256 value: {}", u256_value);
 
-    let hex_value = "0x300239f087b7a581948dc4b14cdc9d5ae13ba8a9cbf56998f764ba13549ac6fa"; // Your hex value
+    // let hex_value = "0x300239f087b7a581948dc4b14cdc9d5ae13ba8a9cbf56998f764ba13549ac6fa"; // Your hex value
 
-    let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
 
-    println!("U256 value: {}", u256_value);
+    // println!("U256 value: {}", u256_value);
+
+    // let hex_value = "0x104d55e131742e10144e0c9023635ee7e000de29d76c05ad56f6a74e40ac9924"; // Your hex value
+
+    // let u256_value = U256::from_str(hex_value).expect("Invalid hex value");
+
+    // println!("U256 value: {}", u256_value);
+    println!("Hello, world!");
 }
 
 
